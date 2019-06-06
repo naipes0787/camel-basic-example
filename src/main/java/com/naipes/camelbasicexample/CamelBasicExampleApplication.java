c4930c6fb3ebae21a880f98643acb6ce323770c9
Leandro Costa <leandro.costa@wolox.com.ar>
2019-06-06 01:00:00 -0300

    Creating the initial structure

---
+++ src/main/java/com/naipes/camelbasicexample/CamelBasicExampleApplication.java
@@ -0,0 +1,13 @@
+package com.naipes.camelbasicexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelBasicExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelBasicExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naipes/camelbasicexample/CamelBasicExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.naipes.camelbasicexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelBasicExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
