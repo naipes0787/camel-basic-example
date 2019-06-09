2a382bd7fb03c229071ca10dbc370ddb72190824
Leandro Costa <leandro.costa@wolox.com.ar>
2019-06-09 02:17:37 -0300

    Adding yml file to the different environments

--- src/main/java/com/naipes/camelbasicexample/route/SimpleCamelRoute.java
+++ src/main/java/com/naipes/camelbasicexample/route/SimpleCamelRoute.java
@@ -1,16 +1,21 @@
 package com.naipes.camelbasicexample.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleCamelRoute extends RouteBuilder {
 
+    @Autowired
+    Environment environment;
+
     @Override
     public void configure() throws Exception {
-        from("timer:hello?period=10s")
-            .log("Timer invoked and the body is: ${body}")
-            .pollEnrich("file:data/input?delete=true&readLock=none")
-            .to("file:data/output");
+        from("{{startRoute}}")
+            .log("Timer invoked and the body is: ${body}. " + environment.getProperty("message"))
+            .pollEnrich("{{fromRoute}}")
+            .to("{{toRoute}}");
     }
 }
---
+++ src/test/java/com/naipes/camelbasicexample/route/SimpleCamelRouteTest.java
@@ -0,0 +1,5 @@
+package com.naipes.camelbasicexample.route;
+
+public class SimpleCamelRouteTest {
+
+}
